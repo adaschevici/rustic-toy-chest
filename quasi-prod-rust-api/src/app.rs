696b317b40b16429b2120fa22ed660a4196c0f23
Artur Das <a.daschevici@gmail.com>
2024-03-06 10:29:56 +0100

    Add opentelemetry

--- quasi-prod-rust-api/src/app.rs
+++ quasi-prod-rust-api/src/app.rs
@@ -21,11 +21,35 @@ pub fn create_app(state: AppState) -> Router {
         .route("/todo/:todo_id", get(get_todo))
         //        .route("/todo/:todo_id", delete(delete_todo))
         //        .route("/todo/:todo_id", put(complete_todo))
-        //        .route("/todo", post(create_todo))
+        .route("/todo", post(create_todo))
         //        .route("/todo", get(get_all_todos))
         //        .route("/todo/random", post(create_random_todo))
         .with_state(Arc::new(state))
         .layer(TraceLayer::new_for_http())
+}
+
+#[instrument]
+async fn create_todo(
+    State(state): State<Arc<AppState>>,
+    Json(new_todo): Json<NewTodo>,
+) -> Result<Json<Todo>, (StatusCode, String)> {
+    let mut conn = state.pool.get().map_err(|_| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Failed to get connection from the pool.".to_string(),
+        )
+    })?;
+    info!("Creating new Todo record in the db: {:?}", &new_todo);
+    let todo = diesel::insert_into(todos::table)
+        .values(&new_todo)
+        .get_result(&mut conn)
+        .map_err(|_| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Failed to create new Todo record.".to_string(),
+            )
+        })?;
+    Ok(Json(todo))
 }
 
 #[instrument]
