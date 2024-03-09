80fadb8e469d7b4148850af58ac971c4e49f56c2
Artur Das <a.daschevici@gmail.com>
2024-03-09 22:46:32 +0100

    Add file read with low level system calls

--- system-calls/src/main.rs
+++ system-calls/src/main.rs
@@ -1,5 +1,10 @@
+use nix::fcntl::{open, OFlag};
 use nix::sys::socket::{socket, AddressFamily, SockFlag, SockType};
-use nix::unistd::{fork, ForkResult};
+use nix::sys::stat::Mode;
+use nix::unistd::Whence;
+use nix::unistd::{close, fork, lseek, read, write, ForkResult};
+use std::fs::File;
+use std::os::unix::io::{FromRawFd, RawFd};
 
 fn main() {
     // TODO: Add error handling and a CLI interface using inquirer-rs and clap
@@ -23,4 +28,29 @@ fn main() {
         None,
     )
     .expect("Failed to create socket");
+    // create a file
+    let path = "tmp.txt";
+    let buffer: &mut [u8] = &mut [0; 256];
+    // open file
+    let fd2: RawFd = open(
+        path,
+        OFlag::O_RDWR | OFlag::O_CREAT,
+        Mode::S_IRUSR | Mode::S_IWUSR,
+    )
+    .expect("Failed to open file");
+
+    // create buffer to write to file
+    let nbytes = unsafe {
+        write(File::from_raw_fd(fd2), b"Hello, nix!\n").expect("Failed to write to file")
+    };
+    let fd2: RawFd = open(
+        path,
+        OFlag::O_RDWR | OFlag::O_CREAT,
+        Mode::S_IRUSR | Mode::S_IWUSR,
+    )
+    .expect("Failed to open file");
+    let _ = lseek(fd2, 0, Whence::SeekSet).expect("Failed to seek to start of file");
+    let _ = read(fd2, buffer).expect("Failed to read from file");
+    close(fd2).expect("Failed to close file");
+    println!("Read from file: {}", String::from_utf8_lossy(buffer));
 }
