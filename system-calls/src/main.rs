6a1c4c8a513bd72b00cd1959669131d392a2cb79
Artur Das <a.daschevici@gmail.com>
2024-03-11 12:23:30 +0100

    Check hostname

--- system-calls/src/main.rs
+++ system-calls/src/main.rs
@@ -4,9 +4,11 @@ use nix::sys::signal::{signal, SigHandler, SIGINT};
 use nix::sys::socket::{socket, AddressFamily, SockFlag, SockType};
 use nix::sys::stat::fchmod as chmod;
 use nix::sys::stat::Mode;
+use nix::unistd::gethostname;
 use nix::unistd::Whence;
 use nix::unistd::{close, fork, getgid, getuid, lseek, pipe, read, write, ForkResult};
 use std::ffi::CString;
+use std::ffi::OsString;
 use std::fs::File;
 use std::os::fd::AsRawFd;
 use std::os::unix::io::{FromRawFd, RawFd};
@@ -110,6 +112,10 @@ fn main() {
     .expect("Failed to open file");
     chmod(fd2, Mode::S_IRUSR | Mode::S_IWUSR).expect("Failed to change file permissions");
     println!("File permissions changed");
+    close(fd2).expect("Failed to close file");
+    // get hostname
+    let hostname: OsString = gethostname().expect("Failed to get hostname");
+    println!("Hostname: {}", hostname.to_string_lossy());
     // add a signal and handler
     unsafe {
         signal(SIGINT, SigHandler::Handler(sigint_handler)).expect("Failed to add signal handler");
