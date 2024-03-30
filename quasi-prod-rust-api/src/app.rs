a10923ddcf78469c1ba4d0f7a696b2d539069184
Artur Das <a.daschevici@gmail.com>
2024-03-31 00:01:23 +0100

    Add reqwest and json for generating random todo

--- quasi-prod-rust-api/src/app.rs
+++ quasi-prod-rust-api/src/app.rs
@@ -23,7 +23,7 @@ pub fn create_app(state: AppState) -> Router {
         //        .route("/todo/:todo_id", put(complete_todo))
         .route("/todo", post(create_todo))
         //        .route("/todo", get(get_all_todos))
-        //        .route("/todo/random", post(create_random_todo))
+        .route("/todo/random", post(create_random_todo))
         .with_state(Arc::new(state))
         .layer(TraceLayer::new_for_http())
 }
@@ -75,4 +75,56 @@ async fn get_todo(
             )
         })?;
     Ok(Json(todo))
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct Activity {
+    pub activity: String,
+    #[serde(alias = "type")]
+    pub activity_type: String,
+}
+
+#[instrument]
+async fn create_random_todo(
+    State(state): State<Arc<AppState>>,
+) -> Result<Json<Todo>, (StatusCode, String)> {
+    let random_activity: Activity = reqwest::get("https://www.boredapi.com/api/activity")
+        .await
+        .map_err(|_| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Failed to get random activity.".to_string(),
+            )
+        })?
+        .json()
+        .await
+        .map_err(|_| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Failed to parse random activity.".to_string(),
+            )
+        })?;
+    info!("Random activity: {:?}", &random_activity);
+
+    let new_todo = NewTodo {
+        title: random_activity.activity,
+        body: random_activity.activity_type,
+    };
+
+    let mut conn = state.pool.get().map_err(internal_error)?;
+    info!("Creating new Todo record in the db: {:?}", &new_todo);
+
+    let res = diesel::insert_into(todos::table)
+        .values(&new_todo)
+        .returning(Todo::as_returning())
+        .get_result(&mut conn)
+        .map_err(internal_error)?;
+    Ok(Json(res))
+}
+
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
