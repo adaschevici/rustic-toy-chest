936797d2f581dcf0c671993718e1808077a80db4
Artur Das <a.daschevici@gmail.com>
2024-04-02 14:42:58 +0200

    Initialize types and variables

--- polling-and-futures/src/main.rs
+++ polling-and-futures/src/main.rs
@@ -1,3 +1,16 @@
+use std::future::Future;
+use std::ptr::null;
+use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
+
+fn wake(_data: *const ()) {}
+fn noop(_data: *const ()) {}
+
+static VTABLE: RawWakerVTable =
+    RawWakerVTable::new(|data| RawWaker::new(data, &VTABLE), wake, wake, noop);
+
 fn main() {
-    println!("Hello, world!");
+    let task = async { 13 };
+    let waker = RawWaker::new(null(), &VTABLE);
+
+    let waker = unsafe { Waker::from_raw(waker) };
 }
