6a9051882bc52e6111d14cca4d2d2363d917ecfa
Artur Das <a.daschevici@gmail.com>
2024-04-02 12:18:05 +0200

    Async read from file

--- async-snippets/src/main.rs
+++ async-snippets/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let result = do_some_io().await?;
+    println!("Result: {}", result);
+    Ok(())
+}
+
+async fn do_some_io() -> Result<String, Box<dyn Error>> {
+    let path = "Cargo.toml";
+    let mut file = File::open(path).await?;
+
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).await?;
+
+    Ok(contents)
 }
