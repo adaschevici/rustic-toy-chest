ab7d50d6831e64ef5b315c89f9992ba6d397ba0d
Artur Das <a.daschevici@gmail.com>
2024-04-03 14:47:09 +0200

    Two step request making

--- one_hundred_kay/src/main.rs
+++ one_hundred_kay/src/main.rs
@@ -2,10 +2,19 @@ use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("http://localhost:4242/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
+    let path = "http://localhost:4242/ip";
+    match reqwest::get(path).await {
+        Ok(response) => match response.text().await {
+            Ok(text) => {
+                println!("RESPONSE: {} bytes from {}", text.len(), path);
+            }
+            Err(_) => {
+                println!("Error reading: {}", path);
+            }
+        },
+        Err(_) => {
+            println!("Error downloading {}", path);
+        }
+    }
     Ok(())
 }
