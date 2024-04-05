8d7731f688a0ce6596187479b33a7093e379bdeb
Artur Das <a.daschevici@gmail.com>
2024-04-05 21:11:08 +0200

    Add serializable message for person capnp object

--- basic_capnp/src/main.rs
+++ basic_capnp/src/main.rs
@@ -1,3 +1,15 @@
+use capnp::message::Builder;
+use capnp::serialize;
+
+pub mod person_capnp;
+
 fn main() {
-    println!("Hello, world!");
+    let mut message = Builder::new_default();
+
+    let mut person = message.init_root::<person_capnp::person::Builder>();
+    person.set_name("John");
+    person.set_age(23);
+
+    let data = serialize::write_message_to_words(&message);
+    println!("{:?}", data);
 }
