8f4d353f1066924d972b500bbe9dd0c4b64c6080
Artur Das <a.daschevici@gmail.com>
2024-04-07 10:27:26 +0200

    Add two tasks that complete at different times, the one that completes first will dictate what the snippet outputs due to the select! macro.

--- select-async/src/main.rs
+++ select-async/src/main.rs
@@ -1,11 +1,23 @@
-use tokio::{self, task};
+use tokio::select;
 
 async fn task_one() -> String {
     tokio::time::sleep(std::time::Duration::from_secs(2)).await;
     "task_one".to_string()
 }
 
+async fn task_two() -> String {
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    "task_two".to_string()
+}
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    select! {
+        res = task_one() => {
+            println!("task_one completed with: {}", res);
+        }
+        res = task_two() => {
+            println!("task_two completed with: {}", res);
+        }
+    }
 }
