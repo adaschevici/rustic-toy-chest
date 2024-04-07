93cf7d6938d0b1ba56dd692dd4aa053f70517da8
Artur Das <a.daschevici@gmail.com>
2024-04-07 10:22:46 +0200

    Add task with small delay

--- select-async/src/main.rs
+++ select-async/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use tokio::{self, task};
+
+async fn task_one() -> String {
+    tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+    "task_one".to_string()
+}
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
