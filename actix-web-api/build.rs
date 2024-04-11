e84fbd573b9b911d34fe44a657722ddd987a28e9
Artur Das <a.daschevici@gmail.com>
2024-04-11 17:36:19 +0200

    Add build step to auto generate the env vars

---
+++ actix-web-api/build.rs
@@ -0,0 +1,41 @@
+use std::env;
+use std::process::Command;
+use std::process::ExitStatus;
+
+fn main() {
+    dotenv::dotenv().ok();
+
+    let mode = match env::var("MODE") {
+        Ok(val) => val,
+        Err(_) => {
+            eprintln!("MODE is not set");
+            return;
+        }
+    };
+
+    // Print a message to stderr
+    Command::new("echo")
+        .arg("setup_env.sh: script started")
+        .status()
+        .expect("failed to run echo");
+
+    let status: ExitStatus;
+
+    if mode == "dev" {
+        status = Command::new("bash")
+            .arg("scripts/setup_env.sh")
+            .status()
+            .expect("Failed to execute Bash script");
+
+        // Check if the script executed successfully
+        if !status.success() {
+            panic!("Failed to run setup_env.sh");
+        }
+
+        if status.success() {
+            eprintln!("retrieved db url");
+        }
+    }
+
+    println!("cargo:rerun-if-changed=build.rs");
+}
