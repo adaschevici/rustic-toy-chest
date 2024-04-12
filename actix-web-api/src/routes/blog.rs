d5b54c8488928b92f84d510d1d8ea19ebfc6d041
Artur Das <a.daschevici@gmail.com>
2024-04-12 16:42:01 +0200

    Update routes

--- actix-web-api/src/routes/blog.rs
+++ actix-web-api/src/routes/blog.rs
@@ -1,12 +1,32 @@
-use crate::models::structs::Blog;
+use crate::models::structs::{Blog, Id};
 use actix_web::{
-    post,
-    web,
-    Error,
-    HttpResponse,
+    delete, get, post, put,
+    web::{Json, Query},
+    Error, HttpResponse,
 };
 
 #[post("/blog")]
-async fn create_blog(data: web::Json<Blog>) -> Result<HttpResponse, Error> {
+async fn create_blog(data: Json<Blog>) -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok().json(data.into_inner()))
+}
+
+#[get("/blog")]
+async fn get_blog_by_id_or_all(Query(id): Query<Id>) -> Result<HttpResponse, Error> {
+    println!("id: {:?}", id);
+    Ok(HttpResponse::Ok().json(id))
+}
+
+#[get("/blog/featured")]
+async fn get_featured_blogs() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().json("featured blogs"))
+}
+
+#[put("/blog")]
+async fn update_blog(data: Json<Blog>) -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().json(data.into_inner()))
+}
+
+#[delete("/blog")]
+async fn delete_blog(id: Json<Id>) -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().json(id.into_inner()))
 }
