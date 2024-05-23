70258d96d11e7af4c29732249e1e3beb2724fafe
Artur Das <a.daschevici@gmail.com>
2024-05-23 16:48:41 +0200

    Add a basic example with tokio but it does not yet seem to be working

--- rust-crawl-pupp/src/main.rs
+++ rust-crawl-pupp/src/main.rs
@@ -1,28 +1,42 @@
 use futures::StreamExt;
+use tokio::task;
 
 use chromiumoxide::{Browser, BrowserConfig};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let browser = Browser::connect(BrowserConfig::builder().build().await.unwrap())
-        .await
-        .unwrap();
+    // create a `Browser` that spawns a `chromium` process running with UI
+    // (`with_head()`, headless is default) and the handler that drives the
+    // websocket etc.
+    let (mut browser, mut handler) =
+        Browser::launch(BrowserConfig::builder().with_head().build()?).await?;
 
-    let tab = browser.new_tab().await.unwrap();
+    // spawn a new task that continuously polls the handler
+    let handle = task::spawn(async move {
+        while let Some(h) = handler.next().await {
+            if h.is_err() {
+                break;
+            }
+        }
+    });
 
-    let response = tab.navigate_to("https://www.rust-lang.org/").await.unwrap();
+    // create a new browser page and navigate to the url
+    let page = browser.new_page("https://en.wikipedia.org").await?;
 
-    let title = response.title().await.unwrap();
-    println!("Title: {}", title);
+    // find the search bar type into the search field and hit `Enter`,
+    // this triggers a new navigation to the search result page
+    page.find_element("input.cdx-text-input__input")
+        .await?
+        .click()
+        .await?
+        .type_str("Rust programming language")
+        .await?
+        .press_key("Enter")
+        .await?;
 
-    let elements = tab.find_elements("a").await.unwrap();
-    let mut count = 0;
-    while let Some(element) = elements.next().await {
-        count += 1;
-        println!("Element: {:?}", element);
-    }
-    println!("Found {} elements", count);
+    let _html = page.wait_for_navigation().await?.content().await?;
 
-    tab.close().await.unwrap();
-    browser.close().await.unwrap();
+    browser.close().await?;
+    handle.await;
+    Ok(())
 }
