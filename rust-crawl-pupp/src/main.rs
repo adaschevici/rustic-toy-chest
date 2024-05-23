371a68bec2ddaa0bc84c07ca6b3de4183410d66c
Artur Das <a.daschevici@gmail.com>
2024-05-23 17:23:04 +0200

    Made several attempts to get a scraper working but it seems to be failing still

--- rust-crawl-pupp/src/main.rs
+++ rust-crawl-pupp/src/main.rs
@@ -1,42 +1,40 @@
 use futures::StreamExt;
-use tokio::task;
 
-use chromiumoxide::{Browser, BrowserConfig};
+use chromiumoxide::browser::{Browser, BrowserConfig};
+use chromiumoxide::page::ScreenshotParams;
+use chromiumoxide_cdp::cdp::browser_protocol::page::CaptureScreenshotFormat;
 
-#[tokio::main]
+#[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // create a `Browser` that spawns a `chromium` process running with UI
-    // (`with_head()`, headless is default) and the handler that drives the
-    // websocket etc.
-    let (mut browser, mut handler) =
-        Browser::launch(BrowserConfig::builder().with_head().build()?).await?;
+    tracing_subscriber::fmt::init();
 
-    // spawn a new task that continuously polls the handler
-    let handle = task::spawn(async move {
-        while let Some(h) = handler.next().await {
-            if h.is_err() {
-                break;
-            }
+    let (browser, mut handler) = Browser::launch(BrowserConfig::builder().build()?).await?;
+
+    let handle = async_std::task::spawn(async move {
+        loop {
+            let _ = handler.next().await.unwrap();
         }
     });
 
-    // create a new browser page and navigate to the url
-    let page = browser.new_page("https://en.wikipedia.org").await?;
+    let page = browser.new_page("https://news.ycombinator.com/").await?;
 
-    // find the search bar type into the search field and hit `Enter`,
-    // this triggers a new navigation to the search result page
-    page.find_element("input.cdx-text-input__input")
+    // take a screenshot of the page
+    page.save_screenshot(
+        ScreenshotParams::builder()
+            .format(CaptureScreenshotFormat::Png)
+            .full_page(true)
+            .omit_background(true)
+            .build(),
+        "hn-page.png",
+    )
+    .await?;
+
+    // get the top post and save a screenshot of it
+    page.find_element("table.itemlist tr")
         .await?
-        .click()
-        .await?
-        .type_str("Rust programming language")
-        .await?
-        .press_key("Enter")
+        .save_screenshot(CaptureScreenshotFormat::Png, "top-post.png")
         .await?;
 
-    let _html = page.wait_for_navigation().await?.content().await?;
-
-    browser.close().await?;
     handle.await;
     Ok(())
 }
