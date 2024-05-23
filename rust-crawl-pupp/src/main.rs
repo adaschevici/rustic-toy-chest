fed29c90df145288df2d2d778bd8eedf10796c15
Artur Das <a.daschevici@gmail.com>
2024-05-23 17:39:38 +0200

    Set a sleep to figure out why it is not finding the element

--- rust-crawl-pupp/src/main.rs
+++ rust-crawl-pupp/src/main.rs
@@ -1,40 +1,38 @@
 use futures::StreamExt;
+use std::{thread, time};
 
 use chromiumoxide::browser::{Browser, BrowserConfig};
-use chromiumoxide::page::ScreenshotParams;
-use chromiumoxide_cdp::cdp::browser_protocol::page::CaptureScreenshotFormat;
 
-#[async_std::main]
+#[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
-    let (browser, mut handler) = Browser::launch(BrowserConfig::builder().build()?).await?;
+    let (browser, mut handler) =
+        Browser::launch(BrowserConfig::builder().with_head().build()?).await?;
 
-    let handle = async_std::task::spawn(async move {
+    let handle = tokio::task::spawn(async move {
         loop {
             let _ = handler.next().await.unwrap();
         }
     });
 
-    let page = browser.new_page("https://news.ycombinator.com/").await?;
+    let page = browser.new_page("https://en.wikipedia.org").await?;
+    let ten_millis = time::Duration::from_millis(50000);
+    let now = time::Instant::now();
 
-    // take a screenshot of the page
-    page.save_screenshot(
-        ScreenshotParams::builder()
-            .format(CaptureScreenshotFormat::Png)
-            .full_page(true)
-            .omit_background(true)
-            .build(),
-        "hn-page.png",
-    )
-    .await?;
+    thread::sleep(ten_millis);
 
-    // get the top post and save a screenshot of it
-    page.find_element("table.itemlist tr")
+    page.find_element("input.cdx-text-input__input")
         .await?
-        .save_screenshot(CaptureScreenshotFormat::Png, "top-post.png")
+        .click()
+        .await?
+        .type_str("Rust programming language")
+        .await?
+        .press_key("Enter")
         .await?;
 
-    handle.await;
+    let _html = page.wait_for_navigation().await?.content().await?;
+
+    handle.await?;
     Ok(())
 }
