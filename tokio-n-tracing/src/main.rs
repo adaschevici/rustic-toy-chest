a40d64d22b81a72f1e43b7144ac02fa59c27aff6
Artur Das <a.daschevici@gmail.com>
2024-06-12 23:09:09 +0200

    Add basic logging and functions

--- tokio-n-tracing/src/main.rs
+++ tokio-n-tracing/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use log::*;
+
+#[tokio::main]
+async fn main() {
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info,myapp=debug");
+    }
+
+    env_logger::init();
+
+    info!("Before calling add");
+    let result = trace_me(5, 3).await;
+    info!("After calling add: {}", result);
+}
+
+async fn trace_me(a: i32, b: i32) -> i32 {
+    debug!("Adding {} and {}", a, b);
+    a + b
 }
