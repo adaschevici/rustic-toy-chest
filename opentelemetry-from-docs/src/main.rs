3023ffc5ef14261291a63ed45fc5f5046869decf
Artur Das <a.daschevici@gmail.com>
2024-06-14 22:02:54 +0200

    Add hello world handler

--- opentelemetry-from-docs/src/main.rs
+++ opentelemetry-from-docs/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let body = Body::from("Hello, World!");
+
+    Ok(Response::new(body))
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    println!("Listening on {addr}");
+    if let Err(e) = server.await {
+        eprintln!("server error: {e}");
+    }
 }
