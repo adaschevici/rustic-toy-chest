277edc1aa0cd4950e868266bc2b90805b144aaa2
Artur Das <a.daschevici@gmail.com>
2024-07-01 00:14:57 +0200

    Fix cdp websocket sending with proper CDP message

--- chrome-raw-websocket-automation/src/main.rs
+++ chrome-raw-websocket-automation/src/main.rs
@@ -1,3 +1,5 @@
+use futures_util::{SinkExt, StreamExt};
+use serde_json::json;
 use tokio::net::TcpStream;
 use tokio_tungstenite::connect_async;
 use tungstenite::protocol::Message;
@@ -6,28 +8,38 @@ use url::Url;
 #[tokio::main]
 async fn main() {
     // Replace with the WebSocket URL of the Chrome browser.
-    let websocket_url = "ws://localhost:9222/devtools/page/your_page_id";
+    let websocket_url = "ws://localhost:9222/devtools/page/AE3DF76262E0BD6382EE2D94B367A1D4";
     let url = Url::parse(websocket_url).expect("Invalid WebSocket URL");
 
     // Connect to the WebSocket server
-    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
-
-    // Split the stream into a sink (to send messages) and a stream (to receive messages)
+    let (ws_stream, _) = connect_async(websocket_url)
+        .await
+        .expect("Failed to connect");
+    //
+    // // Split the stream into a sink (to send messages) and a stream (to receive messages)
     let (mut write, mut read) = ws_stream.split();
-
-    // Send a message to the WebSocket server
-    write.send(Message::Text("Hello WebSocket".into())).await.expect("Failed to send message");
-
-    // Read a message from the WebSocket server
+    let cdp_message = json!({
+        "id": 1,
+        "method": "Page.navigate",
+        "params": {
+            "url": "https://www.example.com"
+        }
+    });
+    //
+    // // Send a message to the WebSocket server
+    write
+        .send(Message::Text(cdp_message.to_string()))
+        .await
+        .expect("Failed to send message");
+    //
+    // // Read a message from the WebSocket server
     if let Some(message) = read.next().await {
         match message {
-            Ok(msg) => {
-                match msg {
-                    Message::Text(text) => println!("Received: {}", text),
-                    Message::Binary(bin) => println!("Received binary data: {:?}", bin),
-                    _ => (),
-                }
-            }
+            Ok(msg) => match msg {
+                Message::Text(text) => println!("Received: {}", text),
+                Message::Binary(bin) => println!("Received binary data: {:?}", bin),
+                _ => (),
+            },
             Err(e) => println!("Error receiving message: {}", e),
         }
     }
