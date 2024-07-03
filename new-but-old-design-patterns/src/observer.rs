6b9584f18b377b102acc10ce81a1b5fbc3446056
Artur Das <a.daschevici@gmail.com>
2024-07-03 23:57:56 +0200

    Add some syncing over threads

--- new-but-old-design-patterns/src/observer.rs
+++ new-but-old-design-patterns/src/observer.rs
@@ -1,29 +1,31 @@
 use async_trait::async_trait;
+use std::sync::{Arc, Mutex};
 use tracing::info;
 
 #[async_trait]
-trait Observer {
-    async fn update(&self);
+trait Observer: Send + Sync {
+    async fn update(&self, event: &ZooEvent);
 }
 
 struct Zoo {
-    observers: Arc<Vec<Box<dyn Observer>>>,
+    observers: Arc<Mutex<Vec<Box<dyn Observer>>>>,
 }
 
 impl Zoo {
     fn new() -> Self {
         Self {
-            observers: Arc::new(Vec::new()),
+            observers: Arc::new(Mutex::new(Vec::new())),
         }
     }
 
-    fn add_observer(&mut self, observer: Box<dyn Observer>) {
-        self.observers.push(observer);
+    async fn add_observer(&mut self, observer: Box<dyn Observer>) {
+        self.observers.lock().unwrap().push(observer);
     }
 
-    async fn notify_observers(&self) {
-        for observer in self.observers.iter() {
-            observer.update().await;
+    async fn notify_observers(&self, event: &ZooEvent) {
+        let observers = self.observers.lock().unwrap();
+        for observer in observers.iter() {
+            observer.update(&event).await;
         }
     }
 }
@@ -40,6 +42,47 @@ impl ZooEvent {
     }
 }
 
+struct Zookeeper {
+    name: String,
+}
+
+#[async_trait]
+impl Observer for Zookeeper {
+    async fn update(&self, event: &ZooEvent) {
+        info!(
+            "Zookeeper {} received an event {}",
+            self.name, event.description
+        );
+    }
+}
+
+struct Veterinarian {
+    name: String,
+}
+
+#[async_trait]
+impl Observer for Veterinarian {
+    async fn update(&self, event: &ZooEvent) {
+        info!(
+            "Veterinarian {} received an event {}",
+            self.name, event.description
+        );
+    }
+}
+
 pub async fn run_observer() {
-    info!("Running observer pattern example");
+    let mut zoo = Zoo::new();
+    let zookeeper = Box::new(Zookeeper {
+        name: "Alice".to_string(),
+    });
+
+    let vet = Box::new(Veterinarian {
+        name: "Bob".to_string(),
+    });
+
+    zoo.add_observer(zookeeper).await;
+    zoo.add_observer(vet).await;
+
+    let event = ZooEvent::new("Tiger escaped");
+    zoo.notify_observers(&event).await;
 }
