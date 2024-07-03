fe7e996ec6932ce3122f2c617b8aa2e6572e8d26
Artur Das <a.daschevici@gmail.com>
2024-07-03 21:23:13 +0200

    Fix state pattern code async doesn't seem to work with traits OOTB

--- new-but-old-design-patterns/src/state_pattern.rs
+++ new-but-old-design-patterns/src/state_pattern.rs
@@ -1,9 +1,9 @@
 use tracing::info;
 
 trait AnimalState {
-    async fn on_enter(&self);
-    async fn on_exit(&self);
-    async fn behavior(&self);
+    fn on_enter(&self);
+    fn on_exit(&self);
+    fn behavior(&self);
 }
 
 struct Sleeping;
@@ -11,43 +11,43 @@ struct Eating;
 struct Playing;
 
 impl AnimalState for Sleeping {
-    async fn on_enter(&self) {
+    fn on_enter(&self) {
         info!("Entering sleeping state");
     }
 
-    async fn on_exit(&self) {
+    fn on_exit(&self) {
         info!("Exiting sleeping state");
     }
 
-    async fn behavior(&self) {
+    fn behavior(&self) {
         info!("Sleeping");
     }
 }
 
 impl AnimalState for Eating {
-    async fn on_enter(&self) {
+    fn on_enter(&self) {
         info!("Entering eating state");
     }
 
-    async fn on_exit(&self) {
+    fn on_exit(&self) {
         info!("Exiting eating state");
     }
 
-    async fn behavior(&self) {
+    fn behavior(&self) {
         info!("Eating");
     }
 }
 
 impl AnimalState for Playing {
-    async fn on_enter(&self) {
+    fn on_enter(&self) {
         info!("Entering playing state");
     }
 
-    async fn on_exit(&self) {
+    fn on_exit(&self) {
         info!("Exiting playing state");
     }
 
-    async fn behavior(&self) {
+    fn behavior(&self) {
         info!("Playing");
     }
 }
@@ -57,23 +57,33 @@ struct Animal {
 }
 
 impl Animal {
-    fn new() -> Self {
+    fn new(initial_state: Box<dyn AnimalState>) -> Self {
+        initial_state.on_enter();
         Self {
-            state: Box::new(Sleeping),
+            state: initial_state,
         }
     }
 
-    async fn change_state(&mut self, new_state: Box<dyn AnimalState>) {
-        self.state.on_exit().await;
+    fn change_state(&mut self, new_state: Box<dyn AnimalState>) {
+        self.state.on_exit();
         self.state = new_state;
-        self.state.on_enter().await;
+        self.state.on_enter();
     }
 
-    async fn run(&self) {
-        self.state.behavior().await;
+    fn exhibit_behavior(&self) {
+        self.state.behavior();
     }
 }
 
 pub async fn run_state_pattern() {
+    let sleeping_state = Box::new(Sleeping);
+    let eating_state = Box::new(Eating);
+    let playing_state = Box::new(Playing);
+    let mut animal = Animal::new(sleeping_state);
+    animal.exhibit_behavior();
+    animal.change_state(eating_state);
+    animal.exhibit_behavior();
+    animal.change_state(playing_state);
+    animal.exhibit_behavior();
     info!("Running state pattern example");
 }
