7170a46d8572ebc7240bb69b615abcd7d55f11df
Artur Das <a.daschevici@gmail.com>
2024-07-04 22:33:34 +0200

    Add the working sync observer code

--- new-but-old-design-patterns/src/observer.rs
+++ new-but-old-design-patterns/src/observer.rs
@@ -1,31 +1,24 @@
-use async_trait::async_trait;
-use std::sync::{Arc, Mutex};
-use tracing::info;
-
-#[async_trait]
-trait Observer: Send + Sync {
-    async fn update(&self, event: &ZooEvent);
+trait Observer {
+    fn update(&self, event: &ZooEvent);
 }
 
 struct Zoo {
-    observers: Arc<Mutex<Vec<Box<dyn Observer>>>>,
+    observers: Vec<Box<dyn Observer>>,
 }
-
 impl Zoo {
-    fn new() -> Self {
-        Self {
-            observers: Arc::new(Mutex::new(Vec::new())),
+    fn new() -> Zoo {
+        Zoo {
+            observers: Vec::new(),
         }
     }
 
-    async fn add_observer(&mut self, observer: Box<dyn Observer>) {
-        self.observers.lock().unwrap().push(observer);
+    fn add_observer(&mut self, observer: Box<dyn Observer>) {
+        self.observers.push(observer);
     }
 
-    async fn notify_observers(&self, event: &ZooEvent) {
-        let observers = self.observers.lock().unwrap();
-        for observer in observers.iter() {
-            observer.update(&event).await;
+    fn notify_observers(&self, event: &ZooEvent) {
+        for observer in &self.observers {
+            observer.update(event);
         }
     }
 }
@@ -33,10 +26,9 @@ impl Zoo {
 struct ZooEvent {
     description: String,
 }
-
 impl ZooEvent {
-    fn new(description: &str) -> Self {
-        Self {
+    fn new(description: &str) -> ZooEvent {
+        ZooEvent {
             description: description.to_string(),
         }
     }
@@ -45,28 +37,17 @@ impl ZooEvent {
 struct Zookeeper {
     name: String,
 }
-
-#[async_trait]
 impl Observer for Zookeeper {
-    async fn update(&self, event: &ZooEvent) {
-        info!(
-            "Zookeeper {} received an event {}",
-            self.name, event.description
-        );
+    fn update(&self, event: &ZooEvent) {
+        println!("{} received an event: {}", self.name, event.description);
     }
 }
-
 struct Veterinarian {
     name: String,
 }
-
-#[async_trait]
 impl Observer for Veterinarian {
-    async fn update(&self, event: &ZooEvent) {
-        info!(
-            "Veterinarian {} received an event {}",
-            self.name, event.description
-        );
+    fn update(&self, event: &ZooEvent) {
+        println!("{} received an event: {}", self.name, event.description);
     }
 }
 
@@ -75,14 +56,13 @@ pub async fn run_observer() {
     let zookeeper = Box::new(Zookeeper {
         name: "Alice".to_string(),
     });
-
     let vet = Box::new(Veterinarian {
         name: "Bob".to_string(),
     });
 
-    zoo.add_observer(zookeeper).await;
-    zoo.add_observer(vet).await;
+    zoo.add_observer(zookeeper);
+    zoo.add_observer(vet);
 
-    let event = ZooEvent::new("Tiger escaped");
-    zoo.notify_observers(&event).await;
+    let event = ZooEvent::new("The lion has been fed.");
+    zoo.notify_observers(&event);
 }
