965ff0ff43dd9924afb1db57aa223fdd691d4119
Artur Das <a.daschevici@gmail.com>
2024-07-07 00:36:56 +0200

    Add facotry and methods

--- new-but-old-design-patterns/src/factory.rs
+++ new-but-old-design-patterns/src/factory.rs
@@ -11,8 +11,36 @@ impl AnimalFactory for LionFactory {
     }
 }
 
+impl AnimalFactory for GiraffeFactory {
+    fn create_animal(&self) -> Box<dyn Animal> {
+        Box::new(Giraffe)
+    }
+}
+
+trait Animal {
+    fn make_sound(&self);
+}
+
+struct Lion;
+struct Giraffe;
+
+impl Animal for Lion {
+    fn make_sound(&self) {
+        println!("Roar!");
+    }
+}
+
+impl Animal for Giraffe {
+    fn make_sound(&self) {
+        println!("Neck snap!");
+    }
+}
+
 pub async fn run_factory() {
     let lion_factory = LionFactory;
+    let giraffe_factory = GiraffeFactory;
     let lion = lion_factory.create_animal();
-    lion.speak().await;
+    let giraffe = giraffe_factory.create_animal();
+    lion.make_sound(); // Outputs: Roar!
+    giraffe.make_sound(); // Outputs: Neck snap!
 }
