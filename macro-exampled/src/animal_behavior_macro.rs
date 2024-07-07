a2bf1681d8c78cc4af1f5a78e9ced45e4488a558
Artur Das <a.daschevici@gmail.com>
2024-07-07 17:08:46 +0200

    Create basic macro

--- macro-exampled/src/animal_behavior_macro.rs
+++ macro-exampled/src/animal_behavior_macro.rs
@@ -1,7 +1,13 @@
+use tracing::info;
+
 macro_rules! animal_behaviour {
     ($animal:expr, $behaviour:expr) => {
         Box::pin(async {
             info!("The {} is {}", $animal, $behaviour);
         })
     };
+}
+
+pub async fn run_animal_behavior_macro() {
+    animal_behaviour!("dog", "barking").await;
 }
--- macro-exampled/src/main.rs
+++ macro-exampled/src/main.rs
@@ -2,20 +2,22 @@ use futures::future::BoxFuture;
 use inquire::Select;
 use tracing::info;
 
-mod basic_macros;
-
-use basic_macros::animal_behaviour;
+mod animal_behavior_macro;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let functions: Vec<(&str, fn() -> BoxFuture<'static, ()>)> =
-        vec![("Run initial macro example", || {
+    let functions: Vec<(&str, fn() -> BoxFuture<'static, ()>)> = vec![
+        ("Run initial macro example", || {
             Box::pin(async {
                 info!("Running initial macro example");
             })
-        })];
+        }),
+        ("Run basic macro example", || {
+            Box::pin(animal_behavior_macro::run_animal_behavior_macro())
+        }),
+    ];
     let function_names: Vec<&str> = functions.iter().map(|(name, _)| *name).collect();
 
     // Prompt the user to select a function
