b907f856cdfc591c23c1635d5df410c5773f39cc
Artur Das <a.daschevici@gmail.com>
2024-07-08 00:17:38 +0200

    Create a simple macro that creates a symbol

--- macro-exampled/src/animal_behavior_macro.rs
+++ macro-exampled/src/animal_behavior_macro.rs
@@ -1,6 +1,6 @@
 use tracing::info;
 
-macro_rules! animal_behaviour {
+macro_rules! animal_behaviour_expr {
     ($animal:expr, $behaviour:expr) => {
         Box::pin(async {
             info!("The {} is {}", $animal, $behaviour);
@@ -8,6 +8,16 @@ macro_rules! animal_behaviour {
     };
 }
 
+macro_rules! animal_behaviour {
+    ($animal:ident, $behaviour:expr) => {
+        fn $animal() {
+            info!("The {} is {}", stringify!($animal), $behaviour);
+        }
+    };
+}
+
 pub async fn run_animal_behavior_macro() {
-    animal_behaviour!("dog", "barking").await;
+    animal_behaviour_expr!("dog", "barking").await;
+    animal_behaviour!(cat, "meowing");
+    cat();
 }
