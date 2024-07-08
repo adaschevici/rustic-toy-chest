6a34c89ff52d4d504cd55b5942dcdcf1fcf89b76
Artur Das <a.daschevici@gmail.com>
2024-07-08 18:57:53 +0200

    Add dependencies

--- macro-exampled/src/procedural_macros.rs
+++ macro-exampled/src/procedural_macros.rs
@@ -1,10 +1,21 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(ToJson)]
 pub fn to_json_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
+    let name = &input.ident;
+    let expanded = quote! {
+        impl ToJson for #name {
+            fn to_json(&self) -> String {
+                format!(
+                    r#"{{ "name": "{}", "age": {}, "is_alive": {} }}"#,
+                    self.name, self.age, self.is_alive
+                )
+            }
+        }
+    };
 }
 
 #[derive(ToJson)]
