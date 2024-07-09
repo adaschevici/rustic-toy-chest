bad891520e1bc2048e7b344de9d4a3a3215e6cdf
Artur Das <a.daschevici@gmail.com>
2024-07-09 09:33:44 +0200

    Make the serializing even more generic

--- workspace_macros/src/lib.rs
+++ workspace_macros/src/lib.rs
@@ -28,14 +28,17 @@ pub fn to_json_generic_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
     let expanded = match input.data {
-        Data::Struct(_) => {
+        Data::Struct(data_struct) => {
+            let fields = data_struct.fields;
+            let field_names = fields.iter().map(|field| &field.ident);
+            let field_strings = field_names
+                .clone()
+                .map(|field| field.as_ref().unwrap().to_string());
             quote! {
                 impl ToJsonGeneric for #name {
                     fn to_json(&self) -> String {
-                        format!(
-                            r#"{{ "name": "{}", "age": {}, "is_alive": {} }}"#,
-                            self.name, self.age, self.is_alive
-                        )
+                        let mut map = ::std::collections::HashMap::new();
+                        #(map.insert(#field_strings.to_string(), ::serde_json::to_value(&self.#field_names).unwrap());)*
                     }
                 }
             }
