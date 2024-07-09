ff7020276c3e8efee8b4dbbd5010ac56496e8030
Artur Das <a.daschevici@gmail.com>
2024-07-09 09:23:13 +0200

    Add generic parsing on the input struct

--- workspace_macros/src/lib.rs
+++ workspace_macros/src/lib.rs
@@ -1,10 +1,10 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput};
 
 mod to_json;
-use to_json::ToJson;
+use to_json::{ToJson, ToJsonGeneric};
 
 #[proc_macro_derive(ToJson)]
 pub fn to_json_derive(input: TokenStream) -> TokenStream {
@@ -17,6 +17,36 @@ pub fn to_json_derive(input: TokenStream) -> TokenStream {
                     r#"{{ "name": "{}", "age": {}, "is_alive": {} }}"#,
                     self.name, self.age, self.is_alive
                 )
+            }
+        }
+    };
+    TokenStream::from(expanded)
+}
+
+#[proc_macro_derive(ToJsonGeneric)]
+pub fn to_json_generic_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = &input.ident;
+    let expanded = match input.data {
+        Data::Struct(_) => {
+            quote! {
+                impl ToJsonGeneric for #name {
+                    fn to_json(&self) -> String {
+                        format!(
+                            r#"{{ "name": "{}", "age": {}, "is_alive": {} }}"#,
+                            self.name, self.age, self.is_alive
+                        )
+                    }
+                }
+            }
+        }
+        _ => {
+            quote! {
+                impl ToJsonGeneric for #name {
+                    fn to_json(&self) -> String {
+                        String::from("Not a struct")
+                    }
+                }
             }
         }
     };
--- workspace_macros/src/to_json.rs
+++ workspace_macros/src/to_json.rs
@@ -1,3 +1,7 @@
 pub trait ToJson {
     fn to_json(&self) -> String;
 }
+
+pub trait ToJsonGeneric {
+    fn to_json(&self) -> String;
+}
