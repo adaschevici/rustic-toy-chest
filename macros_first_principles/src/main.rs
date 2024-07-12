80214dfc7286906b78a0f897a54c1544052f0e9f
Artur Das <a.daschevici@gmail.com>
2024-07-12 19:04:55 +0200

    🚧 Proceed with a silly name extraction
    proceed with a silly name extraction

--- macros_first_principles/src/main.rs
+++ macros_first_principles/src/main.rs
@@ -11,4 +11,12 @@ fn main() {
             y: i32,
         }"#;
     let tokens = TokenStream::from_str(s).unwrap();
+
+    // build the AST: note the syn::parse2() method rather than the syn::parse() one
+    // which is meant for "real" procedural macros
+    let ast: ItemStruct = syn::parse2(tokens).unwrap();
+
+    // save our struct type for later use
+    let struct_type = ast.ident.to_string();
+    println!("struct type: {}", struct_type);
 }
