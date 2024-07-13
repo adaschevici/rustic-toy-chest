741d645b981a9100cb1390ce6a38b65d136059dd
Artur Das <a.daschevici@gmail.com>
2024-07-13 18:11:07 +0200

    🚧 Add a function injection into compiler
    Add the summation function

--- macros_first_principles/src/main.rs
+++ macros_first_principles/src/main.rs
@@ -29,4 +29,27 @@ fn main() {
     let x_field = iter.next().unwrap();
     let x_field_name = x_field.ident.as_ref().unwrap().to_string();
     assert_eq!(x_field_name, "x");
+
+    let y_field = iter.next().unwrap();
+    let y_field_name = y_field.ident.as_ref().unwrap().to_string();
+    assert_eq!(y_field_name, "y");
+
+    // first, build our function name: point_summation
+    let function_name = format_ident!("{}_summation", struct_type.to_lowercase());
+
+    // and our argument type. If we don't use the format ident macro, the function prototype
+    // will be: pub fn point_summation (pt : "Point")
+    let argument_type = format_ident!("{}", struct_type);
+
+    // same for x and y
+    let x = format_ident!("{}", x_field.ident.as_ref().unwrap());
+    let y = format_ident!("{}", y_field.ident.as_ref().unwrap());
+
+    // the quote!() macro is returning a new TokenStream. This TokenStream is returned to
+    // the compiler in a "real" procedural macro
+    let summation_fn = quote! {
+        pub fn #function_name(pt: &#argument_type) -> u16 {
+            pt.#x + pt.#y
+        }
+    };
 }
