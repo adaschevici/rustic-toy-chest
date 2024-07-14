ff7469a109470a48daf238c6c444eda2db275101
Artur Das <a.daschevici@gmail.com>
2024-07-14 23:22:10 +0200

    ✨ Log using tracing
    log using tracing

--- futures-custom/src/perform_basic_request.rs
+++ futures-custom/src/perform_basic_request.rs
@@ -1,4 +1,5 @@
 use reqwest::Error;
+use tracing::info;
 
 pub async fn perform_basic_request() {
     let response = reqwest::get("https://jsonplaceholder.typicode.com/posts/1")
@@ -6,7 +7,7 @@ pub async fn perform_basic_request() {
         .unwrap() // Wait for the request to complete and propagate any errors
         .text() // Convert the response body to text
         .await; // Wait for the text conversion and propagate any errors
-    println!(
+    info!(
         "Response: {}",
         response.unwrap_or_else(|_| "Failed to get response".to_string())
     );
