cd69b9ec9dc2ffb2537de5f1c6e068f1e2640f2b
Artur Das <a.daschevici@gmail.com>
2024-07-14 22:38:49 +0200

    🚧 Add interactive cli
    Add interactive cli

--- futures-custom/src/main.rs
+++ futures-custom/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use inquire::Select;
+use tracing::info;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let functions: Vec<(&str, fn() -> BoxFuture<'static, ()>)> =
+        vec![("Run initial macro example", || {
+            Box::pin(async {
+                info!("Running initial macro example");
+            })
+        })];
+    let function_names: Vec<&str> = functions.iter().map(|(name, _)| *name).collect();
+
+    // Prompt the user to select a function
+    let selected_function = Select::new("Choose a function to execute:", function_names.clone())
+        .with_starting_cursor(function_names.len() - 1)
+        .prompt()
+        .expect("Failed to read input");
+
+    // Find and execute the corresponding function
+    for (name, function) in functions {
+        if name == selected_function {
+            function().await;
+        }
+    }
 }
