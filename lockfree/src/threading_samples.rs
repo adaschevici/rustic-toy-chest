050f9ecd69799095dba28b5e0036b915f45a65fc
Artur Das <a.daschevici@gmail.com>
2024-07-18 15:54:11 +0200

    Finish the atomic shared with single slot

--- lockfree/src/threading_samples.rs
+++ lockfree/src/threading_samples.rs
@@ -17,11 +17,27 @@ pub async fn run_threaded_ops() {
     }
 }
 
-pub async fn run_threaded_with_critical_section() {
-    let val: Arc<AtomicCell<u32>> = Arc::new(AtomicCell::new(0));
+async fn run_thread(val: Arc<AtomicCell<u32>>, num: u32, store: bool) -> thread::JoinHandle<()> {
     thread::spawn(move || {
-        for _ in 0..1000000 {
+        if store {
             val.fetch_add(1);
         }
+        info!("Hello from thread {}! value: {}", num, val.load());
+    })
+}
+
+pub async fn run_threaded_with_critical_section() {
+    info!("Running threaded with critical section");
+    let val: Arc<AtomicCell<u32>> = Arc::new(AtomicCell::new(42));
+    let mut thread_handles_ac: Vec<thread::JoinHandle<()>> = Vec::new();
+    for i in 1..10 {
+        thread_handles_ac.push(run_thread(val.clone(), i, i % 2 == 0).await);
+    }
+
+    thread_handles_ac.into_iter().for_each(|handle| {
+        handle.join().expect("Unable to join thread");
     });
+
+    info!("Final value: {}", val.load());
+    info!("Threaded with critical section complete");
 }
