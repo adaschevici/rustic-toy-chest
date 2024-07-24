fe32f5ae85e77e1624f581180a590c0d07d2d198
Artur Das <a.daschevici@gmail.com>
2024-07-24 09:04:23 +0200

    BAD: this needs another version of tokio

--- thread-rave/src/track_racers.rs
+++ thread-rave/src/track_racers.rs
@@ -1,3 +1,4 @@
+use futures::future::join_all;
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use rand::Rng;
 use rayon::prelude::*;
@@ -5,15 +6,16 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 use std::{io, io::Write};
+use tokio::time::sleep;
 use tracing::info;
 
-fn thread_task(id: usize, max_count: u32, pb: Arc<ProgressBar>) -> usize {
-    let mut rng = rand::thread_rng();
-    let delay = rng.gen_range(1..=50);
+async fn async_thread_task(id: usize, max_count: u32, pb: Arc<ProgressBar>) -> usize {
+    // let mut rng = rand::thread_rng();
+    // let delay = rng.gen_range(1..=50);
 
     for _ in 0..max_count {
         pb.inc(1);
-        thread::sleep(Duration::from_millis(delay as u64));
+        sleep(Duration::from_millis(200 as u64)).await;
     }
     pb.finish_with_message("done");
 
@@ -26,28 +28,36 @@ async fn run_race(
     max_count: u32,
     mp: Arc<MultiProgress>,
 ) -> usize {
-    (0..num_threads).into_par_iter().for_each(|id| {
-        let pb = mp.add(ProgressBar::new(max_count as u64));
-        pb.set_style(
-            ProgressStyle::default_bar()
-                .template(&format!(
-                    "[{{elapsed_precise}}] {{bar:40.cyan/blue}} Thread {}: {{pos}}/{{len}} {{msg}}",
-                    id
-                ))
-                .unwrap()
-                .progress_chars("##-"),
-        );
+    let handles: Vec<_> = (0..num_threads).into_par_iter().map(|id| {
+        let mp = Arc::clone(&mp);
+        let winner = Arc::clone(&winner);
 
-        let pb = Arc::new(pb);
-        let result = thread_task(id, max_count, pb);
+        tokio::spawn(async move {
+            let pb = mp.add(ProgressBar::new(max_count as u64));
+            pb.set_style(
+                ProgressStyle::default_bar()
+                    .template(&format!(
+                        "[{{elapsed_precise}}] {{bar:40.cyan/blue}} Thread {}: {{pos}}/{{len}} {{msg}}",
+                        id
+                    ))
+                    .unwrap()
+                    .progress_chars("##-"),
+            );
+            let pb = Arc::new(pb);
 
-        let mut winner_guard = winner.lock().unwrap();
-        if winner_guard.is_none() {
-            *winner_guard = Some(result);
-        }
-    });
+            let result = async_thread_task(id, max_count, Arc::clone(&pb)).await;
 
-    winner.lock().unwrap().unwrap()
+            let mut winner_guard = winner.lock().unwrap();
+            if winner_guard.is_none() {
+                *winner_guard = Some(result);
+            }
+        })
+    }).collect();
+
+    join_all(handles).await;
+
+    let winner_guard = winner.lock().unwrap();
+    winner_guard.unwrap()
 }
 
 pub async fn run_race_event() {
